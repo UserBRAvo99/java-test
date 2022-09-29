27fefa16998abade9d3676c9126cc63eeff66b6d
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2022-09-29 21:48:42 +0300

    врв

--- js/js.js
+++ js/js.js
@@ -15,17 +15,23 @@
 // const action = prompt('Введи дію',);
 // console.log(first, action, second);
 
-const bananСonnection = Number(prompt('Скільки зв`язок бананів',));
-console.log(`У нас є ${bananСonnection} звязки бананів`)
-const bananPieces = Number(prompt('Скільки бананів в зв`язці',));
-console.log(`У нас є ${bananСonnection} звязки бананів по ${bananPieces} штук.`)
-const strawberries = Number(prompt('Скільки полуничок',));
-console.log(`Полуничок ${strawberries}шт.`)
-const bananMore = bananСonnection * bananPieces;
-console.log(`Всьго бананів ${bananMore} шт.`)
-const strawberriesMore = strawberries - bananMore;
-console.log(strawberriesMore)
-const total = `У нас є ${bananСonnection} звязки бананів по ${bananPieces} штуки. Всьго бананів ${bananMore} шт. Полуничок більше ніж бананів на ${strawberriesMore} шт`;
-console.log(total)
+// const bananСonnection = Number(prompt('Скільки зв`язок бананів',));
+// console.log(`У нас є ${bananСonnection} звязки бананів`)
+// const bananPieces = Number(prompt('Скільки бананів в зв`язці',));
+// console.log(`У нас є ${bananСonnection} звязки бананів по ${bananPieces} штук.`)
+// const strawberries = Number(prompt('Скільки полуничок',));
+// console.log(`Полуничок ${strawberries}шт.`)
+// const bananMore = bananСonnection * bananPieces;
+// console.log(`Всьго бананів ${bananMore} шт.`)
+// const strawberriesMore = strawberries - bananMore;
+// console.log(strawberriesMore)
+// const total = `У нас є ${bananСonnection} звязки бананів по ${bananPieces} штуки. Всьго бананів ${bananMore} шт. Полуничок більше ніж бананів на ${strawberriesMore} шт`;
+// console.log(total)
 
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = (orderedQuantity * pricePerDroid) + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery ( ${deliveryFee} credits) is incuded in total price.`
+// console.log(message)
 
