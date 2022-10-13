67d544d198fcb520f42d84572a8de5ef1a29714d
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2022-10-13 18:10:36 +0300

    10

--- js/module3.js
+++ js/module3.js
@@ -21,4 +21,22 @@
 // apartment.location.country = "Jamaica";
 // apartment.location.city = "Kingston";
 
+// Задача номер 10
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+// };
+// const keys = [];
 
+// const values = [];
+
+// // Change code below this line
+// for (const key in apartment ) {
+//     keys.push(key)
+//     values.push(apartment[key])
+// }
+
+
+// console.log(keys);
+// console.log(values);
