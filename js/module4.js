b2baf606fdb71d36e942183b0d942f7656b46b10
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2022-10-21 22:39:58 +0300

    19

--- js/module4.js
+++ js/module4.js
@@ -339,3 +339,10 @@
 
 // const genres = books.flatMap(book => book.genres);
 // console.log(genres);
+
+// // // // Задача 19
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 === 1);
