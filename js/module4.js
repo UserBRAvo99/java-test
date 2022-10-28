e8d60273a944768d3566aea6ba83cd2f85fb25ce
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2022-10-28 19:32:10 +0300

    45

--- js/module4.js
+++ js/module4.js
@@ -412,3 +412,31 @@
 // };
 // // Change code above this line
 
+// const tweets = [
+//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
+//   { id: "001", likes: 2, tags: ["html", "css"] },
+//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+//   { id: "003", likes: 8, tags: ["css", "react"] },
+//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+// ];
+
+// Пройдемо по всіх елементах колекції і додамо значення властивості tags
+// до акумулятора, початкове значення якого вкажемо порожнім масивом [].
+// На кожній ітерації пушимо в акумулятор усі елементи tweet.tags і повертаємо його.
+// const tags = tweets.reduce((allTags, tweet) => {
+//   allTags.push(...tweet.tags);
+
+//   return allTags;
+// }, []);
+
+// console.log(tags);
+
+// // Мабуть, збирання тегів - не одиночна операція, тому напишемо функцію
+// // для збирання тегів з колекції
+// const getTags = tweets =>
+//   tweets.reduce((allTags, tweet) => {
+//     allTags.push(...tweet.tags);
+
+//     return allTags;
+//   }, []);
+// console.log(getTags(tweets));
